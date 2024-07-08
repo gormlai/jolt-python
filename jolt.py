179f51767ebd75b62ab32bc76e9362cf89f71454
Gorm Lai <723464+gormlai@users.noreply.github.com>
2024-07-08 11:21:52 +0100

    setup simple flow

--- jolt.py
+++ jolt.py
@@ -2,7 +2,25 @@ import sys
 from ctypes import *
 import os
 
-currentDirectory = os.getcwd()
-buildDirectory = currentDirectory + "/build/"
-print("current directory is " + currentDirectory)
-#soFile = currentDirectory + 
+exportedFunctions = None
+
+def init():
+  global exportedFunctions
+  currentDirectory = os.getcwd()
+  buildDirectory = currentDirectory + "/build/"
+  print("current directory is " + currentDirectory)
+  soFile = buildDirectory + "libjolt-python.so"
+  exportedFunctions = CDLL(soFile)
+  exportedFunctions.init()
+
+def start():
+  global exportedFunctions
+  exportedFunctions.start()
+
+def update():
+  global exportedFunctions
+  exportedFunctions.update()
+
+def shutdown():
+  global exportedFunctions
+  exportedFunctions.shutdown()
---
+++ main.py
@@ -0,0 +1,8 @@
+import jolt
+
+if __name__ == "__main__":
+  jolt.init()
+  jolt.start()
+  jolt.update()
+  jolt.shutdown()
+  k=0
