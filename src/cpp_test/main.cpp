e93bc07f06d13c3b9d0089a0f2c7a7779fd677a6
Gorm Lai <723464+gormlai@users.noreply.github.com>
2025-01-06 14:51:28 +0000

    added falling box on top of the sphere

--- src/cpp_test/main.cpp
+++ src/cpp_test/main.cpp
@@ -8,12 +8,12 @@ int main(const int argc, const char **argv) {
   jolt_init();
 
   // first create the ground
-  uint64_t boxShape = jolt_createBoxShape(100.0f, 1.0f, 100.0f);
-  JPH::RVec3 boxPos{0.0, -1.0, 0.0};
-  JPH::Quat boxRotation{0.0, 0.0, 0.0, 1.0};
-  JPH::EMotionType boxMotionType = JPH::EMotionType::Static;
-  JPH::ObjectLayer boxLayer = 0;
-  uint64_t floorBody = jolt_createRigidBody(boxShape, boxPos, boxRotation, boxMotionType, boxLayer);
+  uint64_t floorShape = jolt_createBoxShape(100.0f, 1.0f, 100.0f);
+  JPH::RVec3 floorPos{0.0, -1.0, 0.0};
+  JPH::Quat floorRotation{0.0, 0.0, 0.0, 1.0};
+  JPH::EMotionType floorMotionType = JPH::EMotionType::Static;
+  JPH::ObjectLayer floorLayer = 0;
+  uint64_t floorBody = jolt_createRigidBody(floorShape, floorPos, floorRotation, floorMotionType, floorLayer);
   jolt_addRigidBody(floorBody, false); // do not activate, as the floor does not move
 
   // then a falling sphere
@@ -25,15 +25,41 @@ int main(const int argc, const char **argv) {
   uint64_t sphereBody = jolt_createRigidBody(sphereShape, spherePos, sphereRotation, sphereMotionType, sphereLayer);
   jolt_addRigidBody(sphereBody, true); // activate
 
+  const JPH::Vec3 boxVertices[] = {
+    {-1.0, -1.0, -1.0},
+    {-1.0, 1.0, -1.0},
+    {-1.0, 1.0, 1.0},
+    {-1.0, -1.0, 1.0},
+    {1.0, -1.0, -1.0},
+    {1.0, 1.0, -1.0},
+    {1.0, 1.0, 1.0},
+    {1.0, -1.0, 1.0}
+  };
+  uint64_t boxShape = jolt_createConvexHullShape(boxVertices, sizeof(boxVertices) / sizeof(JPH::Vec3));
+  JPH::RVec3 boxPos{0.2, 5.0, 0.0};
+  JPH::Quat boxRotation{0.0, 0.0, 0.0, 1.0};
+  JPH::EMotionType boxMotionType = JPH::EMotionType::Dynamic;
+  JPH::ObjectLayer boxLayer = 1;
+  uint64_t boxBody = jolt_createRigidBody(boxShape, boxPos, boxRotation, boxMotionType, boxLayer);
+  jolt_addRigidBody(boxBody, true); // activate
+
+
   // start sphere moving
-  JPH::RVec3 sphereVelocity{0.0f, -5.0f, 0.0f};
-  jolt_setLinearVelocity(sphereBody, sphereVelocity);
+  JPH::RVec3 bodyVelocity{0.0f, -5.0f, 0.0f};
+  jolt_setLinearVelocity(sphereBody, bodyVelocity);
+  jolt_setLinearVelocity(boxBody, bodyVelocity);
 
   jolt_start();
-  while(jolt_isActive(sphereBody)) {
-    JPH::RVec3 position = jolt_getCenterOfMassPosition(sphereBody);
-	  JPH::Vec3 velocity = jolt_getLinearVelocity(sphereBody);
-	  std::cout << "Position = (" << position.GetX() << ", " << position.GetY() << ", " << position.GetZ() << "), Velocity = (" << velocity.GetX() << ", " << velocity.GetY() << ", " << velocity.GetZ() << ")" << std::endl;
+
+  while(jolt_isActive(sphereBody) && jolt_isActive(boxBody)) {
+    const JPH::RVec3 spherePosition = jolt_getCenterOfMassPosition(sphereBody);
+	  const JPH::Vec3 sphereVelocity = jolt_getLinearVelocity(sphereBody);
+	  std::cout << "SpherePosition = (" << spherePosition.GetX() << ", " << spherePosition.GetY() << ", " << spherePosition.GetZ() << "), Velocity = (" << sphereVelocity.GetX() << ", " << sphereVelocity.GetY() << ", " << sphereVelocity.GetZ() << ")" << std::endl;
+
+    const JPH::RVec3 boxPosition = jolt_getCenterOfMassPosition(boxBody);
+	  const JPH::Vec3 boxVelocity = jolt_getLinearVelocity(boxBody);
+	  std::cout << "BoxPosition = (" << boxPosition.GetX() << ", " << boxPosition.GetY() << ", " << boxPosition.GetZ() << "), Velocity = (" << boxVelocity.GetX() << ", " << boxVelocity.GetY() << ", " << boxVelocity.GetZ() << ")" << std::endl;
+
     jolt_update();
 
   }
